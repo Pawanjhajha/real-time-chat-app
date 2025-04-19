d1b696625afe3097c2da14fa8faf45fd72aa7e14
pawanjhajha <pawankumarjhajharia3@gmail.com>
2025-04-19 20:03:55 +0530

    fetch the all user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request,Response,NextFunction } from "express";
 import { ApiResponse } from "../common/interface";
 import { userModel } from "../models/user";
+import { Types } from "mongoose";
 
 export const me =async (req:Request,res:Response,next:NextFunction)=>{
     try{
@@ -20,4 +21,37 @@ export const me =async (req:Request,res:Response,next:NextFunction)=>{
         })
     }
 
+}
+export const allUser=async (req:Request,res:Response,next:NextFunction)=>{
+    try{
+        const userId=req.body.userId;
+        //exclude the currently login user
+        const pipeline=[
+            {
+              $match: {
+                _id: {
+                  $ne: Types.ObjectId.createFromHexString(userId)
+                }
+              }
+            },
+            {
+                $project:{
+                    password:0
+                }
+            }
+          ]
+        const users=await userModel.aggregate(pipeline)
+        res.status(200).json(<ApiResponse>{
+            message:"All user fetch",
+            success:true,
+            data:{
+                users:users
+            }
+        })
+    }catch(e){
+        res.status(500).json(<ApiResponse>{
+            message:(e as Error).message,
+            success:true
+        })
+    }
 }
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { me } from '../controllers/userController';
+import { allUser, me } from '../controllers/userController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 const router=express.Router()
 
 router.get('/me',authMiddleware,me)
+router.get('/all-users',authMiddleware,allUser)
 export default router;
